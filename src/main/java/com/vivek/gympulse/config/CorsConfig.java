1fec68ebf3922340964be121d61c96b86a05f388
VB-17-code <vivek.17132@sakec.ac.in>
2026-08-20 21:41:04 +0530

    Update Cors

--- src/main/java/com/vivek/gympulse/config/CorsConfig.java
+++ src/main/java/com/vivek/gympulse/config/CorsConfig.java
@@ -2,57 +2,36 @@ package com.vivek.gympulse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
-@Bean
+    @Bean
+    public WebMvcConfigurer cors() {
 
-public WebMvcConfigurer cors(){
+        return new WebMvcConfigurer() {
 
-return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
 
-@Override
-
-public void addCorsMappings(
-
-CorsRegistry registry
-
-){
-
-registry
-
-.addMapping(
-
-"/**"
-
-)
-
-.allowedOrigins(
-
-//"http://localhost:5173",
-"https://gympulse-frontend.vercel.app"
-
-)
-
-.allowedMethods(
-
-"*"
-
-)
-
-.allowedHeaders(
-
-"*"
-
-);
-
-}
-
-};
-
-}
-
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:5174",
+                        "https://gympulse-frontend.vercel.app"
+                    )
+                    .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                    )
+                    .allowedHeaders("*");
+            }
+        };
+    }
 }
